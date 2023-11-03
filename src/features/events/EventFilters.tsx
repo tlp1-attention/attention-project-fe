9585de7462b56b64af8e8491f1937380ac379831
danteBenitez <benitezdante123@gmail.com>
2023-11-03 13:53:14 -0300

    feat: Filtros de eventos funcionando

--- src/features/events/EventFilters.tsx
+++ src/features/events/EventFilters.tsx
@@ -1,50 +1,57 @@
-import { useSearchParams } from 'react-router-dom';
-import qs from 'qs';
-import { useEffect, useState } from 'react';
+import { useSearchParams } from "react-router-dom";
+import qs from "qs";
+import { useEffect, useState } from "react";
 
 export function EventFilters() {
   const [params, setSearchParams] = useSearchParams();
-  const initialFilter = /filter(\[(\w+)\].+)/.exec(decodeURIComponent(params.toString()))?.[0] ?? '';
-
   const parsed = qs.parse(params.toString());
+  console.log("Parsed: ", parsed);
+  const initialFilter = qs.stringify(
+    { filter: parsed.filter },
+    { encodeValuesOnly: true }
+  );
+  const initialOrder = qs.stringify(
+    { orderField: parsed.orderField, orderType: parsed.orderType },
+    { encodeValuesOnly: true }
+  );
+  console.log("Initial order:", initialOrder);
   const [formValues, setFormValues] = useState({
-    filter: initialFilter.replace('+', '%2b'),
-    order: qs.stringify(parsed.order, { encode: false })
+    filter: initialFilter,
+    order: initialOrder
   });
 
   useEffect(() => {
     const { filter, order } = formValues;
-    if (!filter && !order) return setSearchParams(new URLSearchParams(''));
-    console.log("Filter: ", filter);
-    const urlSearchParams = new URLSearchParams(`${filter}`);
+    if (!filter && !order) return setSearchParams(new URLSearchParams(""));
+    const urlSearchParams = new URLSearchParams(`${filter}&${order}`);
+    console.log(urlSearchParams);
 
-    setSearchParams(
-      urlSearchParams
-    );
+    setSearchParams(urlSearchParams);
+  }, [formValues, setSearchParams]);
 
-  }, [formValues, setSearchParams])
+  console.log(formValues.filter);
 
-  console.log(params.toString());
   return (
     <form
       id="preference-criteria"
       className="d-flex gap-4 p-2 align-items-center"
-      
     >
       <select
         className="btn btn-primary-brand-outline fs-2 form-control"
-        name='filter'
+        name="filter"
         id="filter"
         value={formValues.filter}
-        onChange={(e) => setFormValues({
-          ...formValues,
-          [e.target.name]: e.target.value
-        })}
+        onChange={e =>
+          setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+          })
+        }
       >
         <option value="" selected>
           Filtrar
         </option>
-        <option value="filter[startDate]=%2bnow">Fechas futuras</option>
+        <option value="filter[startDate]=%2Bnow">Fechas futuras</option>
         <option value="filter[typeId]=1">Importantes</option>
         <option value="filter[typeId]=2">No importantes</option>
       </select>
@@ -53,19 +60,21 @@ export function EventFilters() {
         id="sort-criteria"
         name="order"
         value={formValues.order}
-        onChange={(e) => setFormValues({
-          ...formValues,
-          [e.target.name]: e.target.value
-        })}
+        onChange={e =>
+          setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+          })
+        }
       >
-        <option value="">
-          Ordenar por
+        <option value="">Ordenar por</option>
+        <option value="orderField=startDate&orderType=desc">Por fecha</option>
+        <option value="orderField=typeId&orderType=asc">
+          Importantes primero
         </option>
-        <option value="by-date">
-          Por fecha
+        <option value="orderField=typeId&orderType=desc">
+          No importantes primero
         </option>
-        <option value="by-relevance-imp">Importantes primero</option>
-        <option value="by-relevance-no-imp">No importantes primero</option>
       </select>
     </form>
   );
