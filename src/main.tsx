ae4c0723976675d66fb37d029d95d1f71c0bddfe
leonardo <leonardogtaquini@gmail.com>
2023-11-08 16:35:33 -0300

    ultimos cambios

--- src/main.tsx
+++ src/main.tsx
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.min.css';
-import MainLayout from './pages/layouts/MainLayout'
-import HomePage from '@pages/home/Home'
-import LoginForm from '@pages/auth/LoginForm'
-import { Register } from '@pages/auth/Register'
-import WorkspaceLayout from '@pages/layouts/WorkspaceLayout'
-import { UsersPage } from '@pages/users/UsersPage'
-import TimerForm from '@pages/workspace/TimerForm'
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <MainLayout />,
-        errorElement: <p>Something went wrong...</p>,
-        children: [
-            {
-                index: true,
-                element: <HomePage />
-            },
-            {
-                path: '/users',
-                element: <UsersPage />
-            }
-        ]
-    },
-    {
-        path: '/login',
-        element: <LoginForm />,
-    },
-    {
-        path: '/register',
-        element: <Register />
-    },
-    { 
-        path: '/workspace',
-        element: <WorkspaceLayout />,
-        children: [
-            {
-                index: true,
-                element: <TimerForm show={true} />
-            },
-            {
-                
-            }
-        ]
-    }
-])
-=======
 import { AuthContextProvider } from "@features/auth/context/AuthContext";
 import { Toaster } from "@features/ui/toaster/Toaster";
 import LoginForm from "@pages/auth/login/LoginPage";
@@ -71,6 +17,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import MainLayout from "./pages/layouts/MainLayout";
 import { NotFoundPage } from "@pages/NotFoundPage";
+import { UsersPage } from "@pages/users/UsersPage";
+
 
 const router = createBrowserRouter([
   {
@@ -81,13 +29,14 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <HomePage />
-      }
+      },
     ]
   },
   {
     path: "/login",
     element: <LoginForm />
   },
+
   {
     path: "/register",
     element: <Register />
@@ -100,6 +49,10 @@ const router = createBrowserRouter([
         index: true,
         path: "/workspace/timer",
         element: <TimerPage />
+      },
+      {
+        path: "/workspace/users",
+        element: <UsersPage />
       },
       {
         path: "/workspace/events",
@@ -121,6 +74,7 @@ const router = createBrowserRouter([
         path: "/workspace/readings/:readingId/quiz",
         element: <NotFoundPage />,
       },
+
     ]
   },
   {
@@ -128,7 +82,6 @@ const router = createBrowserRouter([
     element: <NotFoundPage />
   }
 ]);
->>>>>>> 29204c2b5e233ab9cd728824e5e428223a400def
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
