25c72151de9c413b985547d7b6d1a202440aa54f
leonardo <leonardogtaquini@gmail.com>
2023-11-08 15:58:48 -0300

    changes added

--- src/features/ui/user-item/UserItem.tsx
+++ src/features/ui/user-item/UserItem.tsx
@@ -1,13 +1,13 @@
 
 import './user-item.css';
 
-export function UserItem({ userInfo:{ name, description }, showUser }: { userInfo: {name: string, description: string}, showUser: ( user: {name: string, description: string}) => void }) {
+export function UserItem({ userInfo:{ name, description, image }, showUser }: { userInfo: {name: string, description: string, image: string}, showUser: ( user: {name: string, description: string, image:string}) => void }) {
 
 
     return (
-        <button className="user-item d-flex align-items-center my-2 userbtn" onClick={ () => showUser( {name, description} ) }>
+        <button className="user-item d-flex align-items-center my-2 userbtn" onClick={ () => showUser( {name, description, image} ) }>
             <div className="icon-container">
-                <img className="image-icon" src="https://static.vecteezy.com/system/resources/previews/019/896/008/original/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png" />
+                <img className="image-icon" src={image} />
             </div>
             <div className=" w-100 text-center">
                 <p className='fw-bolder'>{name}</p>
--- src/pages/users/UsersPage.tsx
+++ src/pages/users/UsersPage.tsx
@@ -4,28 +4,39 @@ import { useState } from 'react';
 
 export function UsersPage() {
 
+    const usersExample = [
+        {name:'User 1', description:'desc1', image: 'https://static.vecteezy.com/system/resources/previews/019/896/008/original/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png'},
+        {name:'User 2', description:'desc2', image: 'https://static.vecteezy.com/system/resources/previews/019/896/008/original/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png'},
+        {name:'User 3', description:'desc3', image: 'https://static.vecteezy.com/system/resources/previews/019/896/008/original/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png'},
+        {name:'User 4', description:'desc4', image: 'https://static.vecteezy.com/system/resources/previews/019/896/008/original/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png'},
+        {name:'User 5', description:'desc5', image: 'https://static.vecteezy.com/system/resources/previews/019/896/008/original/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png'}
+    ];
+
     const [user, setUser] = useState({
         name: '',
-        description: ''
+        description: '',
+        image:''
     });
     
-    const showUser = (user: {name: string, description: string} ): any => {
+    const showUser = (user: {name: string, description: string, image:string} ): any => {
         setUser(user);
     }
 
     return (
-        <div className="users-container border flex-column flex-sm-row flex-lg-row ">
-            <div className='user-list shadow px-3'>
-                <UserItem userInfo={{name:'leo1', description:'desc1'}} showUser={ showUser } />
-                <UserItem userInfo={{name:'leo2', description:'desc2'}} showUser={ showUser } />
-                <UserItem userInfo={{name:'leo3', description:'desc3'}} showUser={ showUser } />
-                <UserItem userInfo={{name:'leo4', description:'desc4'}} showUser={ showUser } />
-                <UserItem userInfo={{name:'leo5', description:'desc5'}} showUser={ showUser } />
-              
+        <div className="users-container border flex-column flex-sm-row flex-lg-row">
+            <div className='user-list shadow px-3' data-bs-spy="scroll" data-bs-target="#list-example" data-bs-smooth-scroll="true" tabIndex={0}>
+                {
+                    usersExample.map((userExample, i) => <UserItem userInfo={ userExample } showUser={ showUser }  key={i}/> )
+                }
             </div>
-            <div className='user w-100'>
-                <h5>{user.name}</h5>
-                <p>{user.description}</p>
+            <div className='user w-100 d-flex p-3'>
+                <div className='col-3 col-sm-6 col-lg-6'>
+                    <img src={user.image} className='img-fluid w-50 m-3' />
+                </div>
+                <div className='col-9 col-sm-6 col-lg-6 d-flex align-items-center flex-column'>
+                    <h3 className='text-capitalize'>{user.name}</h3>
+                    <p>{user.description}</p>
+                </div>
             </div>
         </div>
     )
