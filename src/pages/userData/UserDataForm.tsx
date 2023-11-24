408576e655d3ee48698bc4acf9a635a3ac29af58
EricWithC04 <negativegame117@gmail.com>
2023-11-24 16:11:06 -0300

    feat: logic of form in useForm

--- src/pages/userData/UserDataForm.tsx
+++ src/pages/userData/UserDataForm.tsx
@@ -1,108 +1,16 @@
-import React, { useEffect, useState, useMemo } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { IUser } from '../../interfaces/user'
-import Swt from "sweetalert2"
 import logo from "../../../public/assets/logo-2.png"
+import { useForm } from "@hooks/useForm"
 import "./UserDataForm.css"
 
 const UserDataForm = () => {
 
-    const navigate = useNavigate()
-
-    const [userData, setUserData] = useState<IUser>({
-        name: "",
-        ocupation: "",
-        email: "",
-        description: "",
-    })
-
-    const [errorsActive, setErrorsActive] = useState(false)
-
-    useEffect(() => {
-        const token = localStorage.getItem("token")
-
-        fetch("http://localhost:4000/user/info", {
-            method: "GET",
-            headers: {
-                "content-type": "application/json",
-                "authorization": token ?? ''
-            }
-        })
-            .then(res => res.json())
-            .then(res => {
-                setUserData({
-                    name: res.name ?? "",
-                    ocupation: res.ocupation ?? "",
-                    email: res.email ?? "",
-                    description: res.description ?? "",
-                })
-            })
-            .catch(err => console.error(err))
-    }, [])
-
-    const handleChange = (e: React.ChangeEvent<HTMLFormElement>) => {
-        const newData = {
-            ...userData,
-            [e.target.name]: e.target.value
-        }
-        console.log(newData)
-        setUserData(newData)
-    }
-
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault()
-
-        if (!errorsActive) setErrorsActive(true)
-
-        if (!validateErrors) {
-            const token = localStorage.getItem("token")
-
-            fetch("http://localhost:4000/user/info", {
-                method: "PUT",
-                headers: {
-                    "content-type": "application/json",
-                    "authorization": token ?? ''
-                },
-                body: JSON.stringify(userData),
-            })
-                .then(res => res.json())
-                .then((_res) => {
-                    Swt.fire({
-                        icon: "success",
-                        title: "Datos actualizado correctamente!"
-                    }),
-                        setTimeout(() => {
-                            navigate("/user/profile")
-                        }, 2000);
-                })
-        }
-
-    }
-
-    const regExp = {
-        email: /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/
-    }
-
-    const errors = useMemo(() => {
-        let formErrors = {
-            name: "",
-            ocupation: "",
-            email: "",
-            description: "",
-        }
-        if (!userData.name.length) formErrors.name = "El nombre de usuario no debe estár vacío!"
-        else if (userData.name.length < 8) formErrors.name = "El nombre de usuario es demasiado corto!"
-        else if (userData.name.length > 40) formErrors.name = "El nombre de usuario es demasiado largo!"
-        if (!userData.ocupation.length) formErrors.ocupation = "Debes agregar tu ocupación!"
-        if (!userData.email.length) formErrors.email = "Tu email no puede estár vacío!"
-        else if (!regExp.email.test(userData.email)) formErrors.email = "Debe ser un email valido!"
-
-        return formErrors;
-    }, [userData, errorsActive])
-
-    const validateErrors = useMemo(() => {
-        return Object.values(errors).some(error => error.length > 0);
-    }, [errors])
+    const {
+        handleChange,
+        handleSubmit,
+        userData,
+        errors,
+        errorsActive
+    } = useForm()
 
     return (
         <form
